aeb7b8285687747d4150c7282aca9ef6a7db6488
KPMGE <jesuskevin254@gmail.com>
2023-06-25 21:16:15 -0300

    :sparkles: implement simple health_check route

--- rust-api/src/main.rs
+++ rust-api/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/health_check")]
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(health_check))
+        .bind(("127.0.01", 3333))?
+        .run()
+        .await
 }
