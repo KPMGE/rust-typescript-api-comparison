e16c2bb89f65483d50428bd8df6c31fb615d3c8a
KPMGE <jesuskevin254@gmail.com>
2023-06-27 10:40:37 -0300

    :poop: add dummy create user route, to be refactored

--- rust-api/src/main.rs
+++ rust-api/src/main.rs
@@ -1,10 +1,15 @@
+mod domain;
 mod routes;
 use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(routes::health_check))
-        .bind(("127.0.01", 3333))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(routes::health_check)
+            .service(routes::create_user)
+    })
+    .bind(("127.0.01", 3333))?
+    .run()
+    .await
 }
