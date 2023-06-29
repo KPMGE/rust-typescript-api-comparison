045a724e8acb8f71f10e449b9e3245b3410bbd6f
KPMGE <jesuskevin254@gmail.com>
2023-06-29 11:10:04 -0300

    :recycle: add transaction support

--- typescript-api/src/infra/repositories/todo.ts
+++ typescript-api/src/infra/repositories/todo.ts
@@ -4,47 +4,57 @@ import { Todo } from "../../domain/entities";
 
 const prisma = new PrismaClient()
 
-export class TodoRepository implements 
+export class TodoRepository implements
   CreateTodoRepository,
   ListTodoRepository,
   UpdateTodoRepository,
   DeleteTodoRepository {
 
   async create(userId: number, todo: Omit<Todo, "id">): Promise<void> {
-    await prisma.todo.create({
-      data: {
-        title: todo.title,
-        description: todo.description,
-        completed: false,
-        userId: userId
-      }
-    })
+    await prisma.$transaction([
+      prisma.todo.create({
+        data: {
+          title: todo.title,
+          description: todo.description,
+          completed: false,
+          userId: userId
+        }
+      })
+    ])
   }
 
   async list(userId: number): Promise<Todo[]> {
-    return await prisma.todo.findMany({
-      where: {
-        userId
-      }
-    })
+    const [todos] = await prisma.$transaction([
+      prisma.todo.findMany({
+        where: {
+          userId
+        }
+      })
+    ])
+
+    return todos
   }
 
   async update(todo: Todo): Promise<void> {
-    await prisma.todo.update({
-      where: { id: todo.id },
-      data: {
-        completed: todo.completed,
-        description: todo.description,
-        title: todo.title
-      }
-    })
+    await prisma.$transaction([
+      prisma.todo.update({
+        where: { id: todo.id },
+        data: {
+          completed: todo.completed,
+          description: todo.description,
+          title: todo.title
+        }
+      })
+    ])
   }
 
   async delete(todoId: number): Promise<void> {
-    await prisma.todo.delete({
-      where: {
-        id: todoId
-      }
-    })
+    await prisma.$transaction([
+      prisma.todo.delete({
+        where: {
+          id: todoId
+        }
+      })
+    ])
   }
 }
--- typescript-api/src/infra/repositories/user.ts
+++ typescript-api/src/infra/repositories/user.ts
@@ -1,8 +1,8 @@
 import { PrismaClient } from "@prisma/client";
-import { 
+import {
   CheckUserRepository,
-  CreateUserRepository, 
-  DeleteUserRepository, 
+  CreateUserRepository,
+  DeleteUserRepository,
   ListUserRepository,
   UpdateUserRepository
 } from "../../data/repositories";
@@ -10,55 +10,67 @@ import { User } from "../../domain/entities";
 
 const prisma = new PrismaClient()
 
-export class UserRepository implements 
-  CreateUserRepository, 
-  CheckUserRepository, 
+export class UserRepository implements
+  CreateUserRepository,
+  CheckUserRepository,
   ListUserRepository,
   UpdateUserRepository,
   DeleteUserRepository {
 
   async create(user: User): Promise<void> {
-    await prisma.user.create({
-      data: {
-        name: user.name, 
-        password: user.password, 
-        email: user.email
-      }
-    })
+    await prisma.$transaction([
+      prisma.user.create({
+        data: {
+          name: user.name,
+          password: user.password,
+          email: user.email
+        }
+      })
+    ])
   }
 
   async exists(user: User): Promise<boolean> {
-    const foundUser = await prisma.user.findFirst({
-      where: {
-        name: user.name
-      }
-    })
+    const [foundUser] = await prisma.$transaction([
+      prisma.user.findFirst({
+        where: {
+          name: user.name
+        }
+      })
+    ])
 
-    return !!foundUser 
+    return !!foundUser
   }
 
   async list(): Promise<User[]> {
-    return await prisma.user.findMany()
+    const [users] = await prisma.$transaction([
+      prisma.user.findMany()
+    ])
+
+    return users
   }
 
   async update(userId: number, newUser: Omit<User, 'id'>): Promise<void> {
-    await prisma.user.update({
-      where: {
-        id: userId
-      },
-      data: {
-        name: newUser.name,
-        email: newUser.email,
-      }
-    })
+    await prisma.$transaction([
+      prisma.user.update({
+        where: {
+          id: userId
+        },
+        data: {
+          name: newUser.name,
+          email: newUser.email,
+        }
+      })
+    ])
   }
 
   async delete(userId: number): Promise<void> {
-    await prisma.user.delete({
-      where: {
-        id: userId
-      }
-    })
+    prisma.$transaction([
+      prisma.user.delete({
+        where: {
+          id: userId
+        }
+      })
+    ])
   }
 }
 
